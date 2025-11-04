43efab7b6b04f6249a91fd5a92a0a746d75b17bf
Joso1207 <joso1207@student.miun.se>
2025-11-04 15:10:52 +0100

    Resolve issue from merge conflict

--- src/main/java/org/example/datamodels/BookedInspection.java
+++ src/main/java/org/example/datamodels/BookedInspection.java
@@ -6,13 +6,9 @@ public class BookedInspection extends Booking implements PaidService {
 
     private double price;
 
-<<<<<<< HEAD
-    public BookedInspection(LocalDateTime date, String contactEmail) {
-        super(date, contactEmail);
-=======
+
     public BookedInspection(Vehicle vehicle,LocalDateTime date, String contactEmail) {
-        super(vehicle, date, contactEmail);
->>>>>>> ea201ae4794f017dc3a609a532410a0efcbe3f93
+        super(vehicle ,date, contactEmail);
 
         price = PriceList.inspectionPrice();
     }
