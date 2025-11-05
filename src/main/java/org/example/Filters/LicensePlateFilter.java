fd58b61711956824c299e7e11a07a630024c3ba1
nicolaslovgren92 <nicolas.lovgren92@gmail.com>
2025-11-05 12:02:36 +0100

    Refactor LicensePlateFilter to use the logFactory class

--- src/main/java/org/example/Filters/LicensePlateFilter.java
+++ src/main/java/org/example/Filters/LicensePlateFilter.java
@@ -1,25 +1,40 @@
 package org.example.Filters;
 
-import org.slf4j.Logger;
+
+import org.example.factory.LogFactory;
 import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
 
 import java.util.regex.Pattern;
 
+
+
+
+
 public class LicensePlateFilter {
 
+    private static final Logger logger = LoggerFactory.getLogger(LicensePlateFilter.class);
+
     private static final Pattern LicensePlate = Pattern.compile(
-            "^[A-Z]{3} (?:\\d{3}|\\d{2}[A-Z])$"
-    );
-    private static final Logger log = LoggerFactory.getLogger(LicensePlateFilter.class);
+            "^[A-Z]{3} (?:\\d{3}|\\d{2}[A-Z])$");
+
+
+    public LicensePlateFilter() {
+        LogFactory.loggingContext();
+    }
+
 
     public boolean validateLicensePlate(String plate){
+
+
         if(plate == null || plate.isEmpty()){
-            log.warn("License plate is null or empty");
+           logger.error("License plate is null or empty");
             return false;
         }
         boolean isValid = LicensePlate.matcher(plate).matches();
         if(!isValid){
-            log.warn("Invalid license plate format: {}", plate);
+            logger.error("Invalid license plate format: {}", plate);
         }
         return isValid;
     }
